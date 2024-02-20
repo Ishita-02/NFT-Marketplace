18b4302f23cb84ec8c1debbf7fcd3816fd88d4fe
Ishita-02 <88130678+Ishita-02@users.noreply.github.com>
2024-02-21 00:27:46 +0530

    added images

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,13 +1,2 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract NFTMarketplace is ERC721URIStorage {
-    constructor() ERC721("NFTMarketplace, "NFTH") {
-        owner = payable(msg.sender);
-    } 
-}
